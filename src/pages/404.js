7aa1eeaad0de7d5f17227b10835a3e14fe13d945
Ben Hood <ben@relops.com>
2022-03-09 15:19:45 +0000

    Add a basic 404 catch all page (#1)
    Use a netlify rewrite to handle all bogus requests

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '../components/layout';
+
+const NotFound = () =>
+  <Layout>
+    <h1>Page Not Found</h1>
+    <p>This is not the page you are looking for</p>
+  </Layout>
+
+export default NotFound;
