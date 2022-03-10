916b30133b37f3bb05181e9585d314026459ab50
Ben Hood <ben@relops.com>
2022-03-10 15:26:45 +0000

    Add notice period to membership page

--- src/pages/membership.js
+++ src/pages/membership.js
@@ -25,18 +25,10 @@ const Download = ({name, link}) =>
 const Membership = () =>
   <Layout pageTitle='Membership'>
     <h1>Membership</h1>
-    <h2>Become a Member</h2>
+    <h2>Joining The Club</h2>
     <p>
-        All Skaters taking classes at the Oxford Ice rink are welcome to join once they have achieved their Skate UK bronze level.
+        All skaters are welcome to join once they have achieved their Skate UK bronze level.
     </p>
-
-    <h3>Trial</h3>
-
-    <p>
-      You can start a trial for Figure Club on any Friday that is convenient for you.
-      The trial is not a test, it is just a way to make sure they enjoy the club before committing.
-      The 3 week trial costs £21, payable to the club bank account prior to the first session.
-      Once a skater has completed their 3 week trial, should they enjoy the club, we would ask them to become a permanent member.</p>
 
     <h3>Ice Time</h3>
 
@@ -46,12 +38,30 @@ const Membership = () =>
       Socializing and practising routines is possible too.
     </p>
 
+    <h3>Trial Period</h3>
+
+    <p>
+      You can start a trial for Figure Club on any Friday that is convenient for you.
+      The trial is not a test, it is just a way to make sure they enjoy the club before committing.
+      The 3 week trial costs £21, payable to the club bank account prior to the first session.
+    </p>
+    <p>
+      Once a skater has completed their 3 week trial, should they enjoy the club, we would ask them to become a permanent member.
+    </p>
+
     <h3>Cost</h3>
 
     <p>
-      Club membership costs £28 per month, this covers rink and coach expenses.
-      Club runs most Friday's throughout the year, except Easter and Christmas.
+      Membership costs £28 per month, this covers rink and coach expenses.
+      The club runs most Friday's throughout the year, except Easter and Christmas.
       Siblings receive a 10% discount.
+    </p>
+
+    <h3>Payment</h3>
+
+    <p>
+      Membership fees are due on a monthly rolling basis.
+      When you join, we ask for you to set up a monthly standing order made payable to the club account.
     </p>
 
     <h3>Own Skates</h3>
@@ -66,11 +76,13 @@ const Membership = () =>
 
     <p>All forms are to be mailed to {email}</p>
 
-    <h3>Parent Consent Form</h3>
+    <h3>Parental Consent</h3>
 
     <p>If your child is under the age of 10 years old a responsible adult will need to be inside the building for the entire session, at the end of the sessions a Figure Club member will ensure all children are returned to their parent before releasing them from the rink, if your child is able to leave the rink without supervision, please complete the Parent Consent Form to enable this to happen.</p>
 
     <Download name='Parent Consent Form' link='/registration/OFISC Parent Consent Form.pdf'/>
+
+    <h3>Photography Consent</h3>
 
     <p>
       In accordance with our NISA Safeguarding Policy, we will not permit photographs, video or other images of young people to be taken without the consent of the parents/carers and the child.
@@ -78,6 +90,13 @@ const Membership = () =>
     </p>
 
     <Download name='Photography Consent Form' link='/registration/OFISC Photography Consent Form.pdf'/>
+
+    <h3>Notice Period</h3>
+
+    <p>
+      Should you decide to leave the club, we ask you to advise us one month in advance.
+    </p>
+
 
   </Layout>
 
