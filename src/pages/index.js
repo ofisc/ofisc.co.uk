a2a9d1b6b43d1e59318f6791e4e8176c69e421c6
ClaireBev <159294247+ClaireBev@users.noreply.github.com>
2024-06-02 09:57:19 +0100

    Update index.js (#33)

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,12 @@ import { Layout } from '../components/layout';
 
 const Home = ({data}) => (
   <Layout pageTitle='Home'>
-      <h1>Welcome To The Oxford Figure Ice Skating Club</h1>
-      <p>Oxford Figure Ice Skating Club is a self-funded organisation run by a voluntary committee, with a current membership of 35 skaters. The club is an affiliated member of British Ice Skating, the governing body for ice skating in the UK.</p>
-      <p>We welcome new members up to the age of 18 from Skate UK Bronze Level.</p>
-      <p>OFISC offers skaters the opportunity to train and develop their skills in a supportive, friendly and fun environment and to take part in all club activities.  We have a small team of professional coaches who lead the sessions and provide a fun learning experience.</p>
+      <h1>Welcome To Oxford Figure Ice Skating Club</h1>
+      <p>Oxford Figure Ice Skating Club (OFISC) is a self-funded organisation run by a voluntary committee, with a current membership of 40 skaters. The club is an affiliated member of British Ice Skating, the governing body for ice skating in the UK.</p>
+      <p>We welcome new members up to the age of 18 who have gained Skate UK Gold Level.</p>
+      <p>OFISC offers skaters the opportunity to train and develop their skills in a supportive, friendly and fun environment and to take part in all club activities. We have a small team of professional coaches who lead the sessions and provide a fun learning experience.</p>
       <p>
-        Our regular ice time is on a Friday afternoon at the Oxford Ice Rink from 5:30pm to 6:30pm.
+        Our regular ice time is on a Friday afternoon at Oxford Ice Rink from 5:30pm to 6:30pm.
       </p>
       <div className="pt4">
         <GatsbyImage image={data.file.childImageSharp.gatsbyImageData}/>
